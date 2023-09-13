5e428aea6d7b727599205500227a8c2a41027f5d
chinyishan <star138020@gmail.com>
2023-09-13 22:12:35 +0800

    first

---
+++ pages/about.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div>
+    <h1>About</h1>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template lang="">
+  <div>
+    <h1>首頁</h1>
+  </div>
+</template>
+
+<script lang="ts" setup></script>

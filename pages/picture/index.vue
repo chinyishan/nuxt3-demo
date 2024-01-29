b1b318438f46d23d4de7439c2f16cdd7c3bd42bf
sunny <star138020@gmail.com>
2024-01-29 15:24:37 +0800

    測試 import.meta.glob 動態綁定圖片

--- pages/picture/index.vue
+++ pages/picture/index.vue
@@ -1,16 +1,27 @@
 <template>
   <div>
-    <h1>NuxtImg</h1>
+    <h1>Img 動態綁定路由</h1>
+    <hr />
+    <h2>NuxtImg</h2>
     <ul>
       <li v-for="item in aboutData" :key="item.id">
         <NuxtImg :src="`/${item.pic}`" :alt="item.title" :title="item.title" />
         <p>{{ item.title }}</p>
       </li>
     </ul>
-    <SvgIcon name="icon-globe" fill="#00A53A" stroke="#00A53A"></SvgIcon>
+    <hr />
+    <h2>import.meta.glob</h2>
+    <ul>
+      <li v-for="item in aboutDataTest" :key="item.id">
+        <img :src="images[item.pic]" :alt="item.title" :title="item.title" />
+        <p>{{ item.title }}</p>
+      </li>
+    </ul>
+    <!-- <SvgIcon name="icon-globe" fill="#00A53A" stroke="#00A53A"></SvgIcon>
     <SvgIcon name="earth-world" fill="#00A53A" stroke="#00A53A"></SvgIcon>
     <SvgIcon name="icon-community" color="#00A53A"></SvgIcon>
-    <SvgIcon name="icon-documentation" color="#00A53A"></SvgIcon>
+    <SvgIcon name="icon-documentation" color="#00A53A"></SvgIcon> -->
+    <!-- <img :src="images[dynamic_image_name]" alt="Discover Nuxt 3" /> -->
   </div>
 </template>
 
@@ -34,26 +45,40 @@ const aboutData = reactive([
   },
 ]);
 
-// const aboutData = reactive([
-//   {
-//     id: 1,
-//     title: "About-11",
-//     pic: require("@/assets/images/about-1.jpg"),
-//   },
-//   {
-//     id: 2,
-//     title: "About-22",
-//     pic: require("@/assets/images/about-1.jpg"),
-//   },
-//   {
-//     id: 3,
-//     title: "About-33",
-//     pic: require("@/assets/images/about-1.jpg"),
-//   },
-// ]);
+const aboutDataTest = reactive([
+  {
+    id: 1,
+    title: "About-11",
+    pic: "about-1",
+  },
+  {
+    id: 2,
+    title: "About-22",
+    pic: "about-2",
+  },
+  {
+    id: 3,
+    title: "About-33",
+    pic: "about-3",
+  },
+]);
+
+import { filename } from "pathe/utils";
+
+const glob = import.meta.glob("~/assets/images/*.jpg", { eager: true });
+console.log(glob);
+const images = Object.fromEntries(
+  Object.entries(glob).map(([key, value]) => [filename(key), value.default])
+);
+
+// const dynamic_image_name = aboutDataTest.pic;
+// console.log(dynamic_image_name);
 </script>
 
 <style lang="scss" scoped>
+h2 {
+  text-align: center;
+}
 ul {
   display: flex;
   justify-content: center;
